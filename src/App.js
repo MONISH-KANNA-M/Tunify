b40f57cad88b0432c7c212d23288edb746634b78
MONISH-KANNA-M <monishkanna.m2023aiml@sece.ac.in>
2025-01-30 11:46:03 +0530

    Saved

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Main from "./components/main";
-import Footer from "./components/footer";
+import Footer from "./components/footer"; 
 
 const App = () => {
   return (
@@ -9,7 +9,7 @@ const App = () => {
       <Routes>
         <Route path="/*" element={<Main />} />
       </Routes>
-      <Footer />
+      <Footer /> 
     </>
   );
 };
